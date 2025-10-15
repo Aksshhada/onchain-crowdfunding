303b602a54d077386b6e11e79760648793ecc323
Aksshhada <akshada.kadam088@gmail.com>
2025-10-15 12:11:00 +0530

    second commit

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -11,7 +11,13 @@ contract Crowdfunding {
     constructor(
         string memory _name,
         string memory _description,
-        uint _goal
-        
-    )
+        uint _goal,
+        uint256 _durationInDays
+    ){
+        name = _name;
+        description = _description;
+        goal = _goal;
+        deadline = block.timestamp + (_durationInDays * 1 days);
+        owner = msg.sender;
+    }
 }
