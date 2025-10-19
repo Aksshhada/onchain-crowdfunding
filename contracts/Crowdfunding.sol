afe09b60490d92a092ac1355f946eb061352d652
Aksshhada <akshada.kadam088@gmail.com>
2025-10-19 16:03:28 +0530

    added functions

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.0;
 
 contract Crowdfunding {
     string public name;
-    string public desription;
+    string public description;
     uint256 public goal;
     uint256 public deadline;
     address public owner;
+
+    
 
     constructor(
         string memory _name,
@@ -19,5 +21,24 @@ contract Crowdfunding {
         goal = _goal;
         deadline = block.timestamp + (_durationInDays * 1 days);
         owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(msg.value > 0, "Must fnd amount greater than 0." );
+        require(block.timestamp < deadline, "Campaign has ended.");
+    }
+
+    function withdraw() public {
+        require(msg.sender == owner, "Only owner can withdraw funds.");
+        require(address(this).balance >= goal, "Goal has not been reached.");
+
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No balance to withdraw");
+
+        payable (owner).transfer(balance);
+    }
+
+    function getContractBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }
